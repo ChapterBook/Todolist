3720e078977c23713ed8d88b59096e47484524b3
ChapterBook <truongsach2003@gmail.com>
2024-07-02 13:24:09 +0700

    add abstraction and Retangles

---
+++ Abstraction/Program.cs
@@ -0,0 +1,64 @@
+﻿var retangle1 = new Retangle(5,10);
+var calculor = new ShapesMeasurementsCalculator();
+
+Console.WriteLine("Width is " + retangle1.Width);
+Console.WriteLine("Height is " + retangle1.Height);
+Console.WriteLine("Area is " + calculor.CalculateRetangleArea(retangle1));
+Console.WriteLine("Circumference is " + calculor.CalculateRetangleCircumference(retangle1));
+
+var retangle2 = new Retangle(2, 3);
+
+Console.WriteLine("Width is " + retangle2.Width);
+Console.WriteLine("Height is " + retangle2.Height);
+Console.WriteLine("Area is " + calculor.CalculateRetangleArea(retangle2));
+Console.WriteLine("Circumference is " + calculor.CalculateRetangleCircumference(retangle2));
+
+Console.ReadKey();
+
+class Retangle
+{
+    public int Width = 3; 
+    public int Height = 4;
+
+    public Retangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+}
+class ShapesMeasurementsCalculator
+{
+
+    public int CalculateRetangleArea(Retangle retangle)
+    {
+        return retangle.Width * retangle.Height;
+    }
+
+    public int CalculateRetangleCircumference(Retangle retangle)
+    {
+        return 2 * (retangle.Width +retangle.Height);
+    }
+}
+
+public class Triangle
+{
+    private int _base;
+    private int _height;
+
+    public Triangle(int @base, int height)
+    {
+        _base = @base;
+        _height = height;
+    }
+
+    public int CalculateArea()
+    {
+        return (_base * _height) / 2;
+    }
+    public string AsString()
+    {
+        return $"Base is {_base}, height is {_height}";
+    }
+}
+
---
+++ Retangles/Program.cs
@@ -0,0 +1,36 @@
+﻿var medicalAppointment = new MedicalAppointment("John Smith", new DateTime(2023, 4, 3));
+
+//overwrite month and day
+medicalAppointment.Reschedule(5, 1);
+
+//add a given number of months and days
+medicalAppointment.Reschedule(1, 2);
+
+Console.ReadKey();
+
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+    
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void OverwriteMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void MoreByMonthsAndDays(int monthsToAdd, int daysToAdd)
+    {
+        _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
+    }
+}
